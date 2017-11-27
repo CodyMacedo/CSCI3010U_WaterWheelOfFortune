719b98f99571667af93bcf486399505009cd40cd
yearnath <nathaniel.yearwood@uoit.net>
2017-11-27 17:58:52 -0500

    Added collission detection with center of wheel

--- particle.py
+++ particle.py
@@ -25,7 +25,7 @@ WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
-g = -9.8
+dt = 0.3
 
 
 def normalize(v):
@@ -44,15 +44,17 @@ class Particle(pygame.sprite.Sprite):
         self.mass = mass
         self.t = 0
         self.radius = radius
+        self.gravity = -9.8
 
 
         self.solver = ode(self.f)
         self.solver.set_integrator('dop853')
         self.solver.set_initial_value(self.state, self.t)
+        self.solver.set_f_params(self.gravity)
 
 
-    def f(self, t, y):
-        y[3] = g * self.mass
+    def f(self, t, y, arg1):
+        y[3] += self.mass * arg1/30
         return [y[2], y[3], 0, 0]
 
 
@@ -146,7 +148,7 @@ class World:
         self.wheels =[]
         self.height = height
         self.width = width
-        self.e = 1. # Coefficient of restitution
+        self.e = .1 # Coefficient of restitution
 
 
     def add(self, imgfile, radius, mass=1.0):
@@ -191,7 +193,7 @@ class World:
                 self.particles[i].state[2] *= -1
             elif (self.particles[i].state[1] - self.particles[i].radius <= 0 or
                 self.particles[i].state[1] + self.particles[i].radius >= 600):
-                self.particles[i].state[3] *= -1
+                self.particles[i].state[3] = 0
             else:
                 for j in range(i+1, len(self.particles)):
                     if i == j:
@@ -243,7 +245,52 @@ class World:
 
                     self.particles[i].set_vel(vel_i_aftercollision)
                     self.particles[j].set_vel(vel_j_aftercollision)
-                    break # Only handle a single collision per instance
+                    # break # Only handle a single collision per instance
+
+                # check for particle - wheel collission
+                for j in range(0, len(self.wheels)):
+                    #print 'Checking particles', i, 'and wheel', j
+                    pos_i = np.array(self.particles[i].state[0:2])
+                    pos_j = np.array(self.wheels[j].center)
+                    dist_ij = np.sqrt(np.sum((pos_i - pos_j)**2))
+
+                    radius_i = self.particles[i].radius
+                    radius_j = self.wheels[j].radius*.7
+                    if dist_ij > radius_i + radius_j:
+                        continue
+
+
+                    # May be a collision
+                    vel_i = np.array(self.particles[i].state[2:])
+                    vel_j = 0
+                    relative_vel_ij = vel_i - vel_j
+                    n_ij = normalize(pos_i - pos_j)
+
+
+                    #print relative_vel_ij, n_ij
+
+
+                    if np.dot(relative_vel_ij, n_ij) >= 0:
+                        continue
+
+
+                    # Ouch!
+                    #print 'Collision between particles', i, 'and', j, '!!'
+                    mass_i = self.particles[i].mass
+                    mass_j = self.wheels[j].mass
+
+                    # Don't confuse this J with j
+                    J = -(1+self.e) * np.dot(relative_vel_ij, n_ij) / ((1./mass_i) + (1./mass_j))
+
+
+                    vel_i_aftercollision = vel_i + n_ij * J / mass_i
+
+
+                    #print 'Response'
+                    #print vel_i_aftercollision.shape, vel_j_aftercollision.shape
+
+                    self.particles[i].set_vel(vel_i_aftercollision)
+                    # break # Only handle a single collision per instance
 
 
 
@@ -266,11 +313,11 @@ def main():
 
     world = World(win_height, win_width)
     
-    for i in range(100):
-        newRadius = 3 # smallest radius is 3, anything smaller is invisible
+    for i in range(30):
+        newRadius = 30 # smallest radius is 3, anything smaller is invisible
         newMass = 1
         # make sure particle is within the walls
-        newPos = np.array([rand.uniform(0 + newRadius, 800 - newRadius), rand.uniform(0 + newRadius, 600 - newRadius)])
+        newPos = np.array([rand.uniform(0 + newRadius, win_width - newRadius), rand.uniform(0 + newRadius, win_height - newRadius)])
         newVel = np.array([0, 0])
         
         world.add('sandparticle.png', newRadius, newMass).set_pos(newPos).set_vel(newVel)
@@ -278,7 +325,6 @@ def main():
     world.addWheel([400, 300], 200)
 
    
-    dt = 0.1
     pause = False
 
     while True:
