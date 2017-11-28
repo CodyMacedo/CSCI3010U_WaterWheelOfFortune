8f349143711b07e5bf4e1d0eb66383b197803a83
yearnath <nathaniel.yearwood@uoit.net>
2017-11-27 19:13:54 -0500

    Added timer to create new particles every number of ms

--- particle.py
+++ particle.py
@@ -25,7 +25,9 @@ WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
-dt = 0.3
+
+win_width = 800     # 500 cm = 5 m
+win_height = 600
 
 
 def normalize(v):
@@ -91,6 +93,9 @@ class Particle(pygame.sprite.Sprite):
     def pprint(self):
         print 'Particle', self.state
 
+
+
+
 class Wheel(pygame.sprite.Sprite):
     def __init__(self, center, radius, mass=1000):
         pygame.sprite.Sprite.__init__(self)
@@ -130,7 +135,7 @@ class Wheel(pygame.sprite.Sprite):
 
         self.angle +=1.5
 
-        for i in range(0,359, 45):
+        for i in range(0,316, 45):
             x = self.center[0] + math.cos(math.radians(self.angle + i)) * self.radius
             y = self.center[1] + math.sin(math.radians(self.angle + i)) * self.radius
             self.lines.append(pygame.draw.line(surface, BLACK, self.center, (x,y)))
@@ -140,6 +145,9 @@ class Wheel(pygame.sprite.Sprite):
 
     def pprint(self):
         print 'Wheel', self.state
+
+
+
 
 class World:
 
@@ -155,6 +163,7 @@ class World:
         particle = Particle(imgfile, radius, mass)
         self.particles.append(particle)
         return particle
+
 
     def addWheel(self, centre, radius):
         wheel = Wheel(centre, radius)
@@ -177,6 +186,7 @@ class World:
 
     def update(self, dt):
         self.check_for_collision()
+        self.check_outside_screen()
 
         for d in self.particles:
             d.update(dt)
@@ -186,111 +196,126 @@ class World:
         pass
 
 
+    def check_outside_screen(self):
+        self.particles = [x for x in self.particles if self.outside_screen(x)]
+
+
+    def outside_screen(self, particle):
+        if (particle.state[0] < -particle.radius):
+            return False
+        elif (particle.state[0] > win_width + particle.radius):
+            return False
+        elif (particle.state[1] < -particle.radius):
+            return False
+        else:
+            return True
+
+
     def check_for_collision(self):
         for i in range(0, len(self.particles)):
             if (self.particles[i].state[0] - self.particles[i].radius <= 0 or
                 self.particles[i].state[0] + self.particles[i].radius >= 800):
                 self.particles[i].state[2] *= -1
-            elif (self.particles[i].state[1] - self.particles[i].radius <= 0 or
-                self.particles[i].state[1] + self.particles[i].radius >= 600):
+            elif (self.particles[i].state[1] - self.particles[i].radius <= 0):
                 self.particles[i].state[3] = 0
-            else:
-                for j in range(i+1, len(self.particles)):
-                    if i == j:
-                        continue
-                    #print 'Checking particles', i, 'and', j
-                    pos_i = np.array(self.particles[i].state[0:2])
-                    pos_j = np.array(self.particles[j].state[0:2])
-                    dist_ij = np.sqrt(np.sum((pos_i - pos_j)**2))
+
+            for j in range(i+1, len(self.particles)):
+                if i == j:
+                    continue
+                #print 'Checking particles', i, 'and', j
+                pos_i = np.array(self.particles[i].state[0:2])
+                pos_j = np.array(self.particles[j].state[0:2])
+                dist_ij = np.sqrt(np.sum((pos_i - pos_j)**2))
 
 
-                    #print pos_i, pos_j, dist_ij
+                #print pos_i, pos_j, dist_ij
 
 
-                    radius_i = self.particles[i].radius
-                    radius_j = self.particles[j].radius
-                    if dist_ij > radius_i + radius_j:
-                        continue
+                radius_i = self.particles[i].radius
+                radius_j = self.particles[j].radius
+                if dist_ij > radius_i + radius_j:
+                    continue
 
 
-                    # May be a collision
-                    vel_i = np.array(self.particles[i].state[2:])
-                    vel_j = np.array(self.particles[j].state[2:])
-                    relative_vel_ij = vel_i - vel_j
-                    n_ij = normalize(pos_i - pos_j)
+                # May be a collision
+                vel_i = np.array(self.particles[i].state[2:])
+                vel_j = np.array(self.particles[j].state[2:])
+                relative_vel_ij = vel_i - vel_j
+                n_ij = normalize(pos_i - pos_j)
 
 
-                    #print relative_vel_ij, n_ij
+                #print relative_vel_ij, n_ij
 
 
-                    if np.dot(relative_vel_ij, n_ij) >= 0:
-                        continue
+                if np.dot(relative_vel_ij, n_ij) >= 0:
+                    continue
 
 
-                    # Ouch!
-                    #print 'Collision between particles', i, 'and', j, '!!'
-                    mass_i = self.particles[i].mass
-                    mass_j = self.particles[j].mass
+                # Ouch!
+                #print 'Collision between particles', i, 'and', j, '!!'
+                mass_i = self.particles[i].mass
+                mass_j = self.particles[j].mass
 
-                    # Don't confuse this J with j
-                    J = -(1+self.e) * np.dot(relative_vel_ij, n_ij) / ((1./mass_i) + (1./mass_j))
-
-
-                    vel_i_aftercollision = vel_i + n_ij * J / mass_i
-                    vel_j_aftercollision = vel_j - n_ij * J / mass_j
+                # Don't confuse this J with j
+                J = -(1+self.e) * np.dot(relative_vel_ij, n_ij) / ((1./mass_i) + (1./mass_j))
 
 
-                    #print 'Response'
-                    #print vel_i_aftercollision.shape, vel_j_aftercollision.shape
-
-                    self.particles[i].set_vel(vel_i_aftercollision)
-                    self.particles[j].set_vel(vel_j_aftercollision)
-                    # break # Only handle a single collision per instance
-
-                # check for particle - wheel collission
-                for j in range(0, len(self.wheels)):
-                    #print 'Checking particles', i, 'and wheel', j
-                    pos_i = np.array(self.particles[i].state[0:2])
-                    pos_j = np.array(self.wheels[j].center)
-                    dist_ij = np.sqrt(np.sum((pos_i - pos_j)**2))
-
-                    radius_i = self.particles[i].radius
-                    radius_j = self.wheels[j].radius*.7
-                    if dist_ij > radius_i + radius_j:
-                        continue
+                vel_i_aftercollision = vel_i + n_ij * J / mass_i
+                vel_j_aftercollision = vel_j - n_ij * J / mass_j
 
 
-                    # May be a collision
-                    vel_i = np.array(self.particles[i].state[2:])
-                    vel_j = 0
-                    relative_vel_ij = vel_i - vel_j
-                    n_ij = normalize(pos_i - pos_j)
+                #print 'Response'
+                #print vel_i_aftercollision.shape, vel_j_aftercollision.shape
+
+                self.particles[i].set_vel(vel_i_aftercollision)
+                self.particles[j].set_vel(vel_j_aftercollision)
+                # break # Only handle a single collision per instance
 
 
-                    #print relative_vel_ij, n_ij
+            # check for particle - wheel collission
+            for j in range(0, len(self.wheels)):
+                #print 'Checking particles', i, 'and wheel', j
+                pos_i = np.array(self.particles[i].state[0:2])
+                pos_j = np.array(self.wheels[j].center)
+                dist_ij = np.sqrt(np.sum((pos_i - pos_j)**2))
+
+                radius_i = self.particles[i].radius
+                radius_j = self.wheels[j].radius*.7
+                if dist_ij > radius_i + radius_j:
+                    continue
 
 
-                    if np.dot(relative_vel_ij, n_ij) >= 0:
-                        continue
+                # May be a collision
+                vel_i = np.array(self.particles[i].state[2:])
+                vel_j = 0
+                relative_vel_ij = vel_i - vel_j
+                n_ij = normalize(pos_i - pos_j)
 
 
-                    # Ouch!
-                    #print 'Collision between particles', i, 'and', j, '!!'
-                    mass_i = self.particles[i].mass
-                    mass_j = self.wheels[j].mass
-
-                    # Don't confuse this J with j
-                    J = -(1+self.e) * np.dot(relative_vel_ij, n_ij) / ((1./mass_i) + (1./mass_j))
+                #print relative_vel_ij, n_ij
 
 
-                    vel_i_aftercollision = vel_i + n_ij * J / mass_i
+                if np.dot(relative_vel_ij, n_ij) >= 0:
+                    continue
 
 
-                    #print 'Response'
-                    #print vel_i_aftercollision.shape, vel_j_aftercollision.shape
+                # Ouch!
+                #print 'Collision between particles', i, 'and', j, '!!'
+                mass_i = self.particles[i].mass
+                mass_j = self.wheels[j].mass
 
-                    self.particles[i].set_vel(vel_i_aftercollision)
-                    # break # Only handle a single collision per instance
+                # Don't confuse this J with j
+                J = -(1+self.e) * np.dot(relative_vel_ij, n_ij) / ((1./mass_i) + (1./mass_j))
+
+
+                vel_i_aftercollision = vel_i + n_ij * J / mass_i
+
+
+                #print 'Response'
+                #print vel_i_aftercollision.shape, vel_j_aftercollision.shape
+
+                self.particles[i].set_vel(vel_i_aftercollision)
+                # break # Only handle a single collision per instance
 
 
 
@@ -305,27 +330,19 @@ def main():
 
 
     # top left corner is (0,0)
-    win_width = 800     # 500 cm = 5 m
-    win_height = 600
     screen = pygame.display.set_mode((win_width, win_height))
     pygame.display.set_caption('Water Wheel of Fortune')
 
 
     world = World(win_height, win_width)
-    
-    for i in range(30):
-        newRadius = 30 # smallest radius is 3, anything smaller is invisible
-        newMass = 1
-        # make sure particle is within the walls
-        newPos = np.array([rand.uniform(0 + newRadius, win_width - newRadius), rand.uniform(0 + newRadius, win_height - newRadius)])
-        newVel = np.array([0, 0])
-        
-        world.add('sandparticle.png', newRadius, newMass).set_pos(newPos).set_vel(newVel)
 
     world.addWheel([400, 300], 200)
 
    
     pause = False
+
+    dt = 0.3
+    pygame.time.set_timer(pygame.USEREVENT + 1, 200)
 
     while True:
         # 30 fps
@@ -340,6 +357,16 @@ def main():
             sys.exit(0)
         elif event.type == pygame.KEYDOWN and event.key == pygame.K_p:
             pause = not pause
+        elif event.type == pygame.USEREVENT + 1:
+            # new particle
+            if (len(world.particles) < 100):
+                newRadius = 30 # smallest radius is 3, anything smaller is invisible
+                newMass = 1
+                # make sure particle is within the walls
+                newPos = np.array([rand.uniform(0 + newRadius, win_width - newRadius), win_height])
+                newVel = np.array([0, 0])
+                
+                world.add('sandparticle.png', newRadius, newMass).set_pos(newPos).set_vel(newVel)
         else:
             pass
 
@@ -348,6 +375,7 @@ def main():
             screen.fill(WHITE)
             world.draw(screen)
             world.update(dt)
+            pygame.draw.line(screen, RED, (400, 130), (430, 130))
 
             pygame.display.update()
 
